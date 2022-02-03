102b5b5dc15328bb0dac55a02120679a30df1b9e
Willian Carvalho <Willss_oak@hotmail.com>
2022-02-02 22:35:36 -0300

    adicionando filtros para valor

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const app = new Vue({
   data: {
     produtos: [],
   },
+  filters: {
+    numeroPreco(valor) {
+      return valor.toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+    },
+  },
   methods: {
     fetchProdutos() {
       fetch("/api/produtos.json")
