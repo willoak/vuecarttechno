0760f4612ee5e3216c8d60ade397601b9bda10d1
Willian Carvalho <Willss_oak@hotmail.com>
2022-02-04 00:04:38 -0300

    finalizando site techno em vue, adionando e removendo do carrinho, mostrando parcelamento, ajuste mobile

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = new Vue({
     produtoUnico: false,
     carrinho: [],
     alerta: false,
+    carrinhoAtivo: false,
+    parcelas: 10,
   },
   filters: {
     numeroPreco(valor) {
@@ -39,6 +41,11 @@ const app = new Vue({
         this.alerta = false;
       }
     },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) {
+        this.carrinhoAtivo = false;
+      }
+    },
     adicionarItem() {
       this.produtoUnico.estoque--;
       //desestruturar
@@ -49,6 +56,9 @@ const app = new Vue({
         preco,
       });
       this.alerta = true;
+      setTimeout(() => {
+        this.alerta = false;
+      }, 1500);
     },
     removerItem(index) {
       this.carrinho.splice(index, 1);
@@ -58,14 +68,39 @@ const app = new Vue({
         this.carrinho = JSON.parse(window.localStorage.carrinho);
       }
     },
+    router() {
+      const hash = document.location.hash.replace("#", "");
+      this.fetchProduto(hash);
+    },
+    compararEstoque() {
+      const totalItems = this.carrinho.filter(({ id }) => {
+        // aqui estou verificando se o id do produto que está no carrinho é igual ao produto que estou acessando no momento
+        if (id === this.produtoUnico.id) {
+          return true;
+        }
+      });
+      // aqui eu estou subtraindo do estoque do produto que estou vendo no momento com a quantidade do mesmo que existe no carrinho
+      this.produtoUnico.estoque -= totalItems.length;
+    },
   },
   watch: {
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
     },
+    produtoUnico() {
+      document.title = this.produtoUnico.nome || "Techno";
+
+      //alterando a url
+      const hash = this.produtoUnico.id || "";
+      history.pushState(null, null, `#${hash}`);
+      if (this.produtoUnico) {
+        this.compararEstoque();
+      }
+    },
   },
   created() {
     this.fetchProdutos();
     this.checarLocalStorage();
+    this.router();
   },
 });
